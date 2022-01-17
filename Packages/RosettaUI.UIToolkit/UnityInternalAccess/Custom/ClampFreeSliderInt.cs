a09e3852e50cc9e0611d38677c05befd1e8d598d
fuqunaga <fukhid@gmail.com>
2022-01-17 16:44:16 +0900

    Remove unneeded code

--- Packages/RosettaUI.UIToolkit/UnityInternalAccess/Custom/ClampFreeSliderInt.cs
+++ Packages/RosettaUI.UIToolkit/UnityInternalAccess/Custom/ClampFreeSliderInt.cs
@@ -12,20 +12,5 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
         }
         internal override float SliderNormalizeValue(int currentValue, int lowerValue, int higherValue)
             => Mathf.Clamp01(base.SliderNormalizeValue(currentValue, lowerValue, higherValue));
-
-        protected override void ExecuteDefaultActionAtTarget(EventBase evt)
-        {
-            base.ExecuteDefaultActionAtTarget(evt);
-            
-            // TextField.ExecuteDefaultActionAtTarget()を参考にNavigationを殺す
-            if (evt != null)
-            {
-                if (evt.eventTypeId == EventBase<NavigationMoveEvent>.TypeId())
-                {
-                    evt.StopPropagation();
-                    evt.PreventDefault();
-                }
-            }
-        }
     }
 }

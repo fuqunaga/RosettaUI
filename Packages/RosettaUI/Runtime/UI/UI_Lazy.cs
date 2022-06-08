3f34a790ec1a9fdbe36082970c2397c5fed12753
fuqunaga <fukhid@gmail.com>
2022-06-08 17:53:58 +0900

    add UI_Lazy()

---
+++ Packages/RosettaUI/Runtime/UI/UI_Lazy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RosettaUI
+{
+    public static partial class UI
+    {
+        /// <summary>
+        /// Element to delay build call until Enable==true
+        /// </summary>
+        public static DynamicElement Lazy(Func<Element> build) => DynamicElementIf(() => true, build);
+    }
+}

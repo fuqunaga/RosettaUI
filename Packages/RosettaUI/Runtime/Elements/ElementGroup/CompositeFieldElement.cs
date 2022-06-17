52b563ce2bb013e7622c80d8993cf037be1592ef
fuqunaga <fukhid@gmail.com>
2022-06-17 20:02:55 +0900

    tiny

--- Packages/RosettaUI/Runtime/Elements/ElementGroup/CompositeFieldElement.cs
+++ Packages/RosettaUI/Runtime/Elements/ElementGroup/CompositeFieldElement.cs
@@ -7,16 +7,12 @@ namespace RosettaUI
     /// </summary>
     public class CompositeFieldElement : ElementGroupWithHeader
     {
-        public readonly LabelElement label;
-        
         public CompositeFieldElement(LabelElement label, IEnumerable<Element> contents) : base(label, contents)
         {
             if (label is {labelType: LabelType.Auto})
             {
                 label.labelType = LabelType.Prefix;
             }
-
-            this.label = label;
         }
     }
 }

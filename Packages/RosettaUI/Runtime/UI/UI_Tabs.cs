f175fb37dd35d7a19c677b65095b9b2771141949
fuqunaga <fukhid@gmail.com>
2022-07-04 14:37:04 +0900

    tiny tabs

--- Packages/RosettaUI/Runtime/UI/UI_Tabs.cs
+++ Packages/RosettaUI/Runtime/UI/UI_Tabs.cs
@@ -6,17 +6,17 @@ namespace RosettaUI
 {
     public static partial class UI
     {
-        public static TabsElement Tabs(params (string, Element)[] tabs) => Tabs(tabs.AsEnumerable());
+        public static TabsElement Tabs(params (string label, Element element)[] tabs) => Tabs(tabs.AsEnumerable());
 
-        public static TabsElement Tabs(IEnumerable<(string, Element)> tabs)
+        public static TabsElement Tabs(IEnumerable<(string label, Element element)> tabs)
         {
             return Tabs(tabs.Select(tab => Tab.Create(tab.Item1, tab.Item2)));
         }
 
         // Func<Element> call is delayed until displayed 
-        public static TabsElement Tabs(params (string, Func<Element>)[] tabs)
+        public static TabsElement Tabs(params (string label, Func<Element> createElement)[] tabs)
         {
-            return Tabs(tabs.Select(tab => Tab.Create(tab.Item1, tab.Item2)));
+            return Tabs(tabs.Select(tab => Tab.Create(tab.label, tab.createElement)));
         }
 
         public static TabsElement Tabs(IEnumerable<Tab> tabs) => new(tabs);

2fa8615a6d7f14af0b238e56301c18a53cbc5796
fuqunaga <fukhid@gmail.com>
2022-07-04 15:31:09 +0900

    UI.ScrollView* accepts null size

--- Packages/RosettaUI/Runtime/UI/UI_ScrollView.cs
+++ Packages/RosettaUI/Runtime/UI/UI_ScrollView.cs
@@ -5,22 +5,22 @@ namespace RosettaUI
 {
     public static partial class UI
     {
-        public static ScrollViewElement ScrollViewVertical(float height, params Element[] elements) 
+        public static ScrollViewElement ScrollViewVertical(float? height, params Element[] elements) 
             => ScrollViewVertical(height, elements.AsEnumerable());
 
-        public static ScrollViewElement ScrollViewVertical(float height, IEnumerable<Element> elements)
+        public static ScrollViewElement ScrollViewVertical(float? height, IEnumerable<Element> elements)
             => ScrollView(ScrollViewType.Vertical, elements).SetHeight(height);
 
-        public static ScrollViewElement ScrollViewHorizontal(float width, params Element[] elements) 
+        public static ScrollViewElement ScrollViewHorizontal(float? width, params Element[] elements) 
             => ScrollViewHorizontal(width, elements.AsEnumerable());
         
-        public static ScrollViewElement ScrollViewHorizontal(float width, IEnumerable<Element> elements)
+        public static ScrollViewElement ScrollViewHorizontal(float? width, IEnumerable<Element> elements)
             => ScrollView(ScrollViewType.Horizontal, elements).SetWidth(width);
         
-        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float width, float height, params Element[] elements) 
+        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float? width, float? height, params Element[] elements) 
             => ScrollViewVerticalAndHorizontal(width, height, elements.AsEnumerable());
         
-        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float width, float height, IEnumerable<Element> elements)
+        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float? width, float? height, IEnumerable<Element> elements)
             => ScrollView(ScrollViewType.VerticalAndHorizontal, elements).SetWidth(width).SetHeight(height);
         
         

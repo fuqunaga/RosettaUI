621b85ed8e71e27c337fe71a98fe737d5dc59f22
fuqunaga <fukhid@gmail.com>
2022-07-07 19:51:34 +0900

    fix: indexer will be readonly and strange label. e.g. UI.Field(() => list[0])

--- Assets/Tests/ExpressionUtilityTest.cs
+++ Assets/Tests/ExpressionUtilityTest.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using NUnit.Framework;
 using Assert = UnityEngine.Assertions.Assert;
 
@@ -9,7 +12,8 @@ namespace RosettaUI.Test
         private class Class
         {
             // ReSharper disable once ConvertToConstant.Local
-            public readonly int field = 0;
+            // ReSharper disable once FieldCanBeMadeReadOnly.Local
+            public int field = 0;
             public int Property { get; set; } = 0;
             // ReSharper disable once MemberCanBeMadeStatic.Local
             public int Method() => 0;
@@ -18,6 +22,8 @@ namespace RosettaUI.Test
             // ReSharper disable once UnusedAutoPropertyAccessor.Local
             public static int ClassStaticProperty { get; set; }
             public static int ClassStaticMethod() => 0;
+
+            public List<int> list = new() {0};
         }
         
         public static class StaticClass
@@ -39,12 +45,24 @@ namespace RosettaUI.Test
         private static int StaticProperty { get; set; }
         private static int StaticMethod() => 0;
         
-        
+        private readonly int _readonlyField;
+        private int GetOnlyProperty => 0;
+
+
         Class _instance;
+
+        private int this[int i]
+        {
+            get => 0;
+            set { }
+        }
         
         [Test]
         public void CreateLabelString()
         {
+            var localValue = 0;
+            var list = new List<int>{0};
+            
             // できれば StaticClass.StaticClassField => StaticClass.StaticClassField としたいが、
             // クラス内呼び出しの省略形と区別ができず
             // _staticField => ExpressionUtilityTest._staticField
@@ -57,17 +75,56 @@ namespace RosettaUI.Test
             Assert.AreEqual("_staticField", ExpressionUtility.CreateLabelString(() => _staticField));
             Assert.AreEqual("StaticProperty", ExpressionUtility.CreateLabelString(() => StaticProperty));
             Assert.AreEqual("ExpressionUtilityTest.StaticMethod()", ExpressionUtility.CreateLabelString(() => StaticMethod()));
-
+            
             Assert.AreEqual("_instance.field", ExpressionUtility.CreateLabelString(() => _instance.field));
             Assert.AreEqual("_instance.Property", ExpressionUtility.CreateLabelString(() => _instance.Property));
             Assert.AreEqual("_instance.Method()", ExpressionUtility.CreateLabelString(() => _instance.Method()));
             Assert.AreEqual("classStaticField", ExpressionUtility.CreateLabelString(() => Class.classStaticField));
             Assert.AreEqual("ClassStaticProperty", ExpressionUtility.CreateLabelString(() => Class.ClassStaticProperty));
             Assert.AreEqual("Class.ClassStaticMethod()", ExpressionUtility.CreateLabelString(() => Class.ClassStaticMethod()));
-
+            Assert.AreEqual("_instance.list[0]", ExpressionUtility.CreateLabelString(() => _instance.list[0]));
+            
             Assert.AreEqual("StaticClassField", ExpressionUtility.CreateLabelString(() => StaticClass.StaticClassField));
             Assert.AreEqual("StaticClassProperty", ExpressionUtility.CreateLabelString(() => StaticClass.StaticClassProperty));
             Assert.AreEqual("StaticClass.Method()", ExpressionUtility.CreateLabelString(() => StaticClass.Method()));
+            
+            Assert.AreEqual("localValue", ExpressionUtility.CreateLabelString(() => localValue));
+            Assert.AreEqual("list[0]", ExpressionUtility.CreateLabelString(() => list[0]));
+            Assert.AreEqual("[0]", ExpressionUtility.CreateLabelString(() => this[0]));
+            
+            // 現状非対応
+            // indexerのgetメソッドがget_Itemではない
+            // IndexerNameAttributeでメソッド名取得できる？
+            // Assert.AreEqual("string[0]", ExpressionUtility.CreateLabelString(() => "string"[0]));
         }
+
+
+        [Test]
+        public void CreateBinder()
+        {
+            var list = new List<int> {0};
+            
+            Test(() => 1, typeof(int), true, false); // 現状IsConstになっていない
+            Test(() => _field, typeof(int), false, false);
+            Test(() => _readonlyField, typeof(int), true, false);
+            Test(() => _field + 1, typeof(int), true, false);
+            Test(() => Property, typeof(int), false, false);
+            Test(() => GetOnlyProperty, typeof(int), true, false);
+            Test(() => Method(), typeof(int), true, false);
+            Test(() => list[0], typeof(int), false, false);
+            
+            static void Test<T>(Expression<Func<T>> expression, Type valueType, bool isReadOnly, bool isConst)
+            {
+                var binder = ExpressionUtility.CreateBinder(expression);
+                Assert.AreEqual(
+                    (binder.ValueType, binder.IsReadOnly, binder.IsConst),
+                    (valueType, isReadOnly, isConst),
+                    $"Expression: {ExpressionUtility.CreateLabelString(expression)}"
+                );
+            }
+        }
+
+
+
     }
 }
--- Packages/RosettaUI/Runtime/Utilities/ExpressionUtility.cs
+++ Packages/RosettaUI/Runtime/Utilities/ExpressionUtility.cs
@@ -5,9 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using UnityEngine;
 using UnityEngine.Assertions;
-
 
 namespace RosettaUI
 {
@@ -47,12 +45,25 @@ namespace RosettaUI
                 ? (Expression) p
                 : Expression.Convert(p, bodyType);
 
+            // indexer対策
+            // UI.Field(() => list[0]);
+            // は expression.Body は list.Item プロパティのGetMethodなので ReadOnly になってしまう
+            // expression.Body がプロパティのGetMethodで、SetMethodが存在するならそちらに差し替える
+            var setBody = expression.Body;
+            if (setBody is MethodCallExpression mc)
+            {
+                if (TryGetIndexerPropertyInfoFromGetMethod(mc, out var pi) && pi.SetMethod != null)
+                {
+                    setBody = Expression.Property(mc.Object, pi, mc.Arguments);
+                }
+            }
+
 
             // check writable
             // hint code https://stackoverflow.com/questions/42773488/how-can-i-find-out-if-an-expression-is-writeable
             try
             {
-                var lambdaExpr = Expression.Lambda<Action<T>>(Expression.Assign(expression.Body, rightHand), p);
+                var lambdaExpr = Expression.Lambda<Action<T>>(Expression.Assign(setBody, rightHand), p);
                 setFunc = lambdaExpr.Compile();
             }
             catch (Exception)
@@ -69,8 +80,9 @@ namespace RosettaUI
 
         const string MethodCallDummyInstanceName = "RosettaUI_MethodCallDummy...";
         const string StaticMethodCallDummyClassName = "RosettaUI_StaticMethodCallDummy";
+        const string IndexerDummyInstanceName = "RosettaUI_IndexerDummyInstance";
         private static ToLabelStringVisitor _readableExpressionVisitor;
-        private static Regex _staticMethodDummyRegex;
+
 
         public static string CreateLabelString<T>(Expression<Func<T>> expression)
         {
@@ -86,10 +98,22 @@ namespace RosettaUI
             
             str = str.Replace(MethodCallDummyInstanceName + ".", "");
 
-            _staticMethodDummyRegex ??= new Regex(@"\(" + StaticMethodCallDummyClassName + @"(\w+) = (\w+.*)\)");
-            str = _staticMethodDummyRegex.Replace(str, "$1.$2");
-            
-            if (str[0] == '(' && str.IndexOf(')') == str.Length - 1)
+            // Replace static method
+            // StaticMethodCallDummyClassName + StaticClass = StaticMethod()  -> StaticClass.StaticMethod()
+            str = Regex.Replace(str,
+                @"\(" + StaticMethodCallDummyClassName + @"(\w+) = (\w+.*)\)",
+                "$1.$2");
+
+            // Replace indexer
+            // IndexerDummyInstanceName = list.get_item(i) -> list[i]
+            str = Regex.Replace(str, 
+                @"\(" + IndexerDummyInstanceName + @" = (.*)\.get_Item\((.*)\)\)",
+                "$1[$2]");
+            str = Regex.Replace(str, 
+                @"\(" + IndexerDummyInstanceName + @" = get_Item\((.*)\)\)",
+                "[$1]");
+
+            if (str[0] == '(' && str[^1] == ')')
             {
                 str = str[1..^1];
             }
@@ -134,32 +158,54 @@ namespace RosettaUI
 
 
             /// <summary>
-            /// 1. Remove complex "this" string
-            /// 
-            /// Warning: Constant is not only "this"
+            /// 1. Add static class name
             ///
-            /// 2. Add static class name
+            /// 2. Indexer
+            ///   list.get_item(0) -> list[0]
+            ///
+            /// 3. Remove complex "this" string
+            ///    Warning: Constant is not only "this"
+            ///
             /// </summary>
             protected override Expression VisitMethodCall(MethodCallExpression node)
             {
                 var obj = node.Object;
+                var method = node.Method;
 
                 // add static class name
-                if ( node.Method.IsStatic )
+                // 一度Assignの形にしてあとで置換する
+                // StaticMethod() -> StaticMethodCallDummyClassName+"StaticClass" = StaticMethod() 
+                if ( method.IsStatic )
                 {
-                    var type = node.Method.ReturnType;
-                    var param = Expression.Parameter(type, StaticMethodCallDummyClassName + node.Method.DeclaringType.Name);
+                    var param = Expression.Parameter(node.Type, StaticMethodCallDummyClassName + method.DeclaringType?.Name);
                     return Expression.Assign(param, node);
+                }
+   
+                // Indexer
+                // 一度Assignの形にしてあとで置換する
+                // list.get_item(i) -> IndexerDummyInstanceName = list.get_item(i) 
+                if ( TryGetIndexerPropertyInfoFromGetMethod(node, out _))
+                {
+                    var param = Expression.Parameter(node.Type, IndexerDummyInstanceName);
+                    return Expression.Assign(param, RemoveInstanceNameIfConstant(node));
                 }
                 
                 // remove "this"
+                return RemoveInstanceNameIfConstant(node);
+            }
+            
+                          
+            Expression RemoveInstanceNameIfConstant(MethodCallExpression expression)
+            {
+                var obj = expression.Object;
+                    
                 if (obj is ConstantExpression)
                 {
                     var param = Expression.Parameter(obj.Type, MethodCallDummyInstanceName);
-                    return Expression.Call(param, node.Method, node.Arguments);
+                    return Expression.Call(param, expression.Method, expression.Arguments);
                 }
-
-                return base.VisitMethodCall(node);
+                
+                return base.VisitMethodCall(expression);
             }
         }
 
@@ -181,5 +227,14 @@ namespace RosettaUI
         }
 
         #endregion
+
+        static bool TryGetIndexerPropertyInfoFromGetMethod(MethodCallExpression mc, out PropertyInfo pi)
+        {
+            var mi = mc.Method;
+            var binding = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            pi = mi.DeclaringType?.GetProperties(binding).FirstOrDefault(info => info.GetMethod == mi);
+
+            return pi != null;
+        }
     }
 }

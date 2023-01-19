2ae059fab27d78febdaff3767d810db6988bcde6
fuqunaga <fukhid@gmail.com>
2023-01-19 19:02:12 +0900

    Window間でフォーカスが移ってしまう挙動を抑制（一度消したけど必要だった）

--- Packages/RosettaUI.UIToolkit/Runtime/VisualElement/Window.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/VisualElement/Window.cs
@@ -196,6 +196,11 @@ namespace RosettaUI.UIToolkit
                 ResetFixedSize();
             });
 
+            // Focusable.ExecuteDefaultEvent() 内の this.focusController?.SwitchFocusOnEvent(evt) で
+            // NavigationMoveEvent 方向にフォーカスを移動しようとする
+            // キー入力をしている場合などにフォーカスが移ってしまうのは避けたいのでWindow単位で抑制しておく
+            // UnityデフォルトでもTextFieldは抑制できているが、IntegerField.inputFieldでは出来ていないなど挙動に一貫性がない
+            RegisterCallback<NavigationMoveEvent>(evt => evt.PreventDefault());
 
             ResetFixedSize();
         }

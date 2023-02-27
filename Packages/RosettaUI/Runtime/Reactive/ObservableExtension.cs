5400b7b6687649561c0635dc96f3c13f3364059d
fuqunaga <fukhid@gmail.com>
2023-02-27 17:06:56 +0900

    fix: compile error when Api Compatibility Level == .NET framework

--- Packages/RosettaUI/Runtime/Reactive/ObservableExtension.cs
+++ Packages/RosettaUI/Runtime/Reactive/ObservableExtension.cs
@@ -5,11 +5,9 @@ namespace RosettaUI.Reactive
 {
     public static class ObservableExtension
     {
-#if !NET_4_6
         public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext)
         {
             return source.Subscribe(new Observer<T>(onNext));
         }
-#endif
     }
 }

306ea864348b18d3f331feeadfe5872740151fcb
fuqunaga <fukhid@gmail.com>
2023-06-21 18:37:31 +0900

    fix: Changes to the value of items in the list were not notified outside the UI.
    The Setter in the ListBinder was not being called.
    So, libraries such as PrefsGUI were not receiving notification of changes to the List.

---
+++ Packages/RosettaUI/Runtime/Binder/ListItemBinder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace RosettaUI
+{
+    public class ListItemBinder<T> : ChildBinder<IList<T>, T>, IListItemBinder
+    {
+        private static IBinder<IList<T>> CreateIListBinder(IBinder binder)
+        {
+            return (IBinder<IList<T>>)CastBinder.Create(binder, binder.ValueType, typeof(IList<T>));
+        }
+        
+        public int Index { get; set; }
+        
+        public ListItemBinder(IBinder listBinder, int index) : base(CreateIListBinder(listBinder))
+        {
+            Index = index;
+        }
+
+        protected override T GetFromParent(IList<T> list) => (0 <= Index && Index < list.Count) ? list[Index] : default;
+        protected override IList<T> SetToParent(IList<T> list, T value)
+        {
+            if ( Index < list.Count ) list[Index] = value;
+            return list; 
+        }
+    }
+}

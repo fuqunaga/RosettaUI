26744173576195fb5dc68bc168d91977c39bdc17
fuqunaga <fukhid@gmail.com>
2024-05-23 16:50:25 +0900

    supports UIToolkitUtility.SetAcceptClicksIfDisabled()

--- Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/UIToolkitUtility.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/UIToolkitUtility.cs
@@ -1,15 +1,36 @@
+using System.Reflection;
 using UnityEngine.UIElements;
 
 namespace RosettaUI.UIToolkit.UnityInternalAccess
 {
     public  static class UIToolkitUtility
     {
+#if UNITY_2022_1_OR_NEWER
+        
+        private static PropertyInfo _baseBoolFieldAcceptClicksIfDisabledPropertyInfo;
+        
         public static bool WillUseKeyInput(IPanel panel)
         {
-#if UNITY_2022_1_OR_NEWER
             // TODO
             return false;
+        }
+
+        public static void SetAcceptClicksIfDisabled(BaseBoolField baseBoolField, bool flag = true)
+        {
+            _baseBoolFieldAcceptClicksIfDisabledPropertyInfo ??= typeof(BaseBoolField).GetProperty("acceptClicksIfDisabled", BindingFlags.NonPublic | BindingFlags.Instance);
+            _baseBoolFieldAcceptClicksIfDisabledPropertyInfo?.SetValue(baseBoolField, flag);
+        }
+
+        public static void RegisterCallbackIncludeDisabled<TEventType>(VisualElement ve, EventCallback<TEventType> callback)
+            where TEventType : EventBase<TEventType>, new()
+        {
+
+        }
+        
 #else
+        
+        public static bool WillUseKeyInput(IPanel panel)
+        {
 #if UNITY_2022_1_OR_NEWER
             // refs: TextInputBaseField.hasFocus, TextElement.hasFocus 
             var element = panel?.focusController?.GetLeafFocusedElement();
@@ -27,22 +48,18 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
             var element = panel?.focusController?.GetLeafFocusedElement();
             return element is ITextInputField {hasFocus: true};
 #endif
-#endif
         }
 
         public static void SetAcceptClicksIfDisabled(BaseBoolField baseBoolField, bool flag = true)
         {
-#if !UNITY_2022_1_OR_NEWER
             baseBoolField.m_Clickable.acceptClicksIfDisabled = flag;
-#endif
         }
 
         public static void RegisterCallbackIncludeDisabled<TEventType>(VisualElement ve, EventCallback<TEventType> callback)
             where TEventType : EventBase<TEventType>, new()
         {
-#if !UNITY_2022_1_OR_NEWER
             ve.RegisterCallback(callback, InvokePolicy.IncludeDisabled);
-#endif
         }
+#endif
     }
 }

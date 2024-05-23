39a107010f400f79c0c90e5203b2fcd5faf164b8
fuqunaga <fukhid@gmail.com>
2024-05-23 16:49:31 +0900

    support PopupField at runtime

--- Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/DropDownMenuGenerator.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/DropDownMenuGenerator.cs
@@ -8,12 +8,28 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
     {
         public static object Generate(IEnumerable<MenuItem> menuItems, Rect position, VisualElement targetElement = null, bool anchored = false)
         {
-#if UNITY_2021_2_OR_NEWER
-            // TODO: Implement for Unity 2022.1 or newer
-            return null;
-#else
             if (menuItems == null) return null;
+#if UNITY_2023_1_OR_NEWER
+            // refs: BasePopupField
+            // https://github.com/Unity-Technologies/UnityCsReference/blob/c84064be69f20dcf21ebe4a7bbc176d48e2f289c/ModuleOverrides/com.unity.ui/Core/Controls/BasePopupField.cs#L206
+            var isPlayer = targetElement?.panel.contextType == ContextType.Player;
 
+            // TODO: On editor
+            
+            // see: DropdownUtility, EditorDelegateRegistration, GenericOSMenu
+            // var menu = isPlayer
+            //     ? new GenericDropdownMenu()
+            //     : new UnityEditor.GenericMenu();
+            var menu = new GenericDropdownMenu();
+
+            foreach (var item in menuItems)
+            {
+                if (item.isEnable) menu.AddItem(item.name, item.isChecked, item.action);
+                else menu.AddDisabledItem(item.name, item.isChecked);
+            }
+
+            menu.DropDown(position, targetElement, anchored);
+#else
             // refs: BasePopupField
             // https://github.com/Unity-Technologies/UnityCsReference/blob/c84064be69f20dcf21ebe4a7bbc176d48e2f289c/ModuleOverrides/com.unity.ui/Core/Controls/BasePopupField.cs#L206
             var isPlayer = targetElement?.elementPanel.contextType == ContextType.Player;
@@ -29,9 +45,8 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
             }
 
             menu.DropDown(position, targetElement, anchored);
-
-            return menu;
 #endif
+            return menu;
         }
     }
 }

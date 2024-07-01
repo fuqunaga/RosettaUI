b0152f735948e653b6536d20a232743d8bb63e1d
fuqunaga <fukhid@gmail.com>
2024-07-01 14:44:36 +0900

    DropDownMenuGenerator supports unity2023 or newer

--- Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/DropDownMenuGenerator.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/DropDownMenuGenerator.cs
@@ -9,26 +9,13 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
         public static object Generate(IEnumerable<MenuItem> menuItems, Rect position, VisualElement targetElement = null, bool anchored = false)
         {
             if (menuItems == null) return null;
-#if UNITY_2023_1_OR_NEWER
-            // refs: BasePopupField
-            // https://github.com/Unity-Technologies/UnityCsReference/blob/c84064be69f20dcf21ebe4a7bbc176d48e2f289c/ModuleOverrides/com.unity.ui/Core/Controls/BasePopupField.cs#L206
-            var isPlayer = targetElement?.panel.contextType == ContextType.Player;
-
-            // TODO: On editor
             
-            // see: DropdownUtility, EditorDelegateRegistration, GenericOSMenu
+#if UNITY_2023_1_OR_NEWER
+            // refs: DropdownUtility, EditorDelegateRegistration, GenericOSMenu
             // var menu = isPlayer
             //     ? new GenericDropdownMenu()
             //     : new UnityEditor.GenericMenu();
             var menu = new GenericDropdownMenu();
-
-            foreach (var item in menuItems)
-            {
-                if (item.isEnable) menu.AddItem(item.name, item.isChecked, item.action);
-                else menu.AddDisabledItem(item.name, item.isChecked);
-            }
-
-            menu.DropDown(position, targetElement, anchored);
 #else
             // refs: BasePopupField
             // https://github.com/Unity-Technologies/UnityCsReference/blob/c84064be69f20dcf21ebe4a7bbc176d48e2f289c/ModuleOverrides/com.unity.ui/Core/Controls/BasePopupField.cs#L206
@@ -37,6 +24,7 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
             var menu = isPlayer
                 ? new GenericDropdownMenu()
                 : DropdownUtility.CreateDropdown();
+#endif
 
             foreach (var item in menuItems)
             {
@@ -45,7 +33,6 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
             }
 
             menu.DropDown(position, targetElement, anchored);
-#endif
             return menu;
         }
     }

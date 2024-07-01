8ab6c213e40619e22eee009e83113c539a174eb6
fuqunaga <fukhid@gmail.com>
2024-07-01 14:53:27 +0900

    UIToolkitUtility supports unity2022 and 2023orNewer

--- Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/UIToolkitUtility.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/UnityInternalAccess/Custom/UIToolkitUtility.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UIElements;
 
@@ -7,10 +6,10 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
 {
     public  static class UIToolkitUtility
     {
-#if UNITY_2022_1_OR_NEWER
-        
         private static MethodInfo _focusControllerGetLeafFocusedElementMethodInfo;
+#if UNITY_2023_1_OR_NEWER
         private static PropertyInfo _baseBoolFieldAcceptClicksIfDisabledPropertyInfo;
+#endif
         
         public static bool WillUseKeyInput(IPanel panel)
         {
@@ -27,49 +26,12 @@ namespace RosettaUI.UIToolkit.UnityInternalAccess
 
         public static void SetAcceptClicksIfDisabled(BaseBoolField baseBoolField, bool flag = true)
         {
+#if UNITY_2023_1_OR_NEWER
             _baseBoolFieldAcceptClicksIfDisabledPropertyInfo ??= typeof(BaseBoolField).GetProperty("acceptClicksIfDisabled", BindingFlags.NonPublic | BindingFlags.Instance);
             _baseBoolFieldAcceptClicksIfDisabledPropertyInfo?.SetValue(baseBoolField, flag);
-        }
-
-        public static void RegisterCallbackIncludeDisabled<TEventType>(VisualElement ve, EventCallback<TEventType> callback)
-            where TEventType : EventBase<TEventType>, new()
-        {
-
-        }
-        
 #else
-        
-        public static bool WillUseKeyInput(IPanel panel)
-        {
-#if UNITY_2022_1_OR_NEWER
-            // refs: TextInputBaseField.hasFocus, TextElement.hasFocus 
-            var element = panel?.focusController?.GetLeafFocusedElement();
-            return (element is TextElement textElement) &&
-                   (
-                       // Single line
-                       (textElement.parent?.name == TextInputBaseField<string>.textInputUssName) ||
-                       
-                       // Multi line
-                       // ref: TextInputBase.SetMultiline()
-                       (textElement.parent?.parent?.name == TextInputBaseField<string>.textInputUssName)
-                   );
-#else
-            // refs: TextInputBase.hasFocus
-            var element = panel?.focusController?.GetLeafFocusedElement();
-            return element is ITextInputField {hasFocus: true};
-#endif
-        }
-
-        public static void SetAcceptClicksIfDisabled(BaseBoolField baseBoolField, bool flag = true)
-        {
             baseBoolField.m_Clickable.acceptClicksIfDisabled = flag;
-        }
-
-        public static void RegisterCallbackIncludeDisabled<TEventType>(VisualElement ve, EventCallback<TEventType> callback)
-            where TEventType : EventBase<TEventType>, new()
-        {
-            ve.RegisterCallback(callback, InvokePolicy.IncludeDisabled);
-        }
 #endif
+        }
     }
 }

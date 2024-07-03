fd3b1ba64510bdd18f12cdc1e7777bd2a5096a1d
fuqunaga <fukhid@gmail.com>
2024-07-03 18:19:09 +0900

    add Checkerboard visualelement

---
+++ Packages/RosettaUI.UIToolkit/Runtime/VisualElement/Checkerboard.cs
@@ -0,0 +1,28 @@
+﻿using RosettaUI.Builder;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace RosettaUI.UIToolkit
+{
+    public class Checkerboard : VisualElement
+    {
+        public const string UssClassName = "rosettaui-checkerboard";
+
+        public static Texture2D CheckerBoardTexture2X2 { get; private set; }
+        
+
+        public Checkerboard(int size = 12, Texture2D texture = null)
+        {
+            AddToClassList(UssClassName);
+
+            if (texture == null)
+            {
+                CheckerBoardTexture2X2 ??= TextureUtility.CreateCheckerBoardTexture(new Vector2Int(2, 2), 1);
+                texture = CheckerBoardTexture2X2;
+            }
+
+            style.backgroundImage = texture;
+            style.backgroundSize = new BackgroundSize(size, size);
+        }
+    }
+}

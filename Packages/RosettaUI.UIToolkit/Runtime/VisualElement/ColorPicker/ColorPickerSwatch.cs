8e284ae683a2282bb2477ad428b80e858effc154
fuqunaga <fukhid@gmail.com>
2024-07-05 15:30:08 +0900

    current swatch style

--- Packages/RosettaUI.UIToolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatch.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatch.cs
@@ -7,30 +7,50 @@ namespace RosettaUI.UIToolkit
     public class ColorPickerSwatch : VisualElement
     {
         public const string UssClassName = "rosettaui-colorpicker-swatch";
-        public const string ColorUssClassName = UssClassName + "__color";
-        public const string ColorOpaqueUssClassName = UssClassName + "__color-opaque";
+        public const string CurrentClassName = UssClassName + "-current";
+        public const string CoreClassName = UssClassName + "-core";
+        public const string ColorUssClassName = UssClassName + "-color";
+        public const string ColorOpaqueUssClassName = UssClassName + "-color-opaque";
 
+        private readonly VisualElement _coreElement;
         private readonly VisualElement _colorElement;
         private readonly VisualElement _colorOpaque;
-        
-        
 
+        public bool IsCurrent
+        {
+            set
+            {
+                if (value)
+                {
+                    AddToClassList(CurrentClassName);
+                }
+                else
+                {
+                    RemoveFromClassList(CurrentClassName);
+                }
+                
+            }
+        }
+        
         public Color Color
         {
             get => _colorElement.style.backgroundColor.value;
             set
             {
                 _colorElement.style.backgroundColor = value;
-                
+
                 var colorWithoutAlpha = value;
                 colorWithoutAlpha.a = 1;
                 _colorOpaque.style.unityBackgroundImageTintColor = colorWithoutAlpha;
             }
-    }
+        }
         
         public ColorPickerSwatch()
         {
             AddToClassList(UssClassName);
+            
+            _coreElement = new VisualElement();
+            _coreElement.AddToClassList(CoreClassName);
             
             var checkerboardElement = new Checkerboard(CheckerboardTheme.Light);
 
@@ -42,7 +62,11 @@ namespace RosettaUI.UIToolkit
 
             _colorElement.Add(_colorOpaque);
             checkerboardElement.Add(_colorElement);
-            Add(checkerboardElement);
+            _coreElement.Add(checkerboardElement);
+            
+            Add(_coreElement);
+
+            IsCurrent = true;
         }
     }
 }

09aff3dae73e9c4e4e144d06ed09e5e3e82ab004
fuqunaga <fukhid@gmail.com>
2024-07-08 19:21:27 +0900

    support Swatch name

--- Packages/RosettaUI.UIToolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatch.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatch.cs
@@ -1,4 +1,5 @@
-﻿using RosettaUI.Builder;
+﻿using System;
+using RosettaUI.Builder;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,16 +8,19 @@ namespace RosettaUI.UIToolkit
     public class ColorPickerSwatch : VisualElement
     {
         public const string UssClassName = "rosettaui-colorpicker-swatch";
+        public const string EnableTextUssClassName = UssClassName + "--enable-text";
         public const string TileClassName = UssClassName + "__tile";
         public const string TileCurrentClassName = TileClassName + "-current";
         public const string TileCoreClassName = TileClassName + "-core";
         public const string TileColorUssClassName = TileClassName + "-color";
         public const string TileColorOpaqueUssClassName = TileClassName + "-color-opaque";
+        public const string TextContainerUssClassName = UssClassName + "__text-container";
+        public const string TextInputUssClassName = TextContainerUssClassName + "__text-input";
         
         private readonly VisualElement _tileElement;
         private readonly VisualElement _colorElement;
         private readonly VisualElement _colorOpaque;
-
+        private readonly VisualElement _textContainer;
         private Label _label;
 
         public bool IsCurrent
@@ -49,22 +53,28 @@ namespace RosettaUI.UIToolkit
 
         public string Label
         {
+            get => _label?.text;
             set
             {
                 if (string.IsNullOrEmpty(value))
                 {
-                    if ( _label != null) Remove(_label);
+                    if ( _label != null) _textContainer.Remove(_label);
                 }
                 else
                 {
                     _label ??= new Label();
 
                     _label.text = value;
-                    Add(_label);
+                    _textContainer.Add(_label);
                 }
             }
         }
-        
+
+        public bool EnableText
+        {
+            set => EnableInClassList(EnableTextUssClassName, value);
+        }
+
         public ColorPickerSwatch()
         {
             AddToClassList(UssClassName);
@@ -88,7 +98,61 @@ namespace RosettaUI.UIToolkit
             coreElement.Add(checkerboardElement);
             _tileElement.Add(coreElement);
             
+            _textContainer = new VisualElement();
+            _textContainer.AddToClassList(TextContainerUssClassName);
+            
             Add(_tileElement);
+            Add(_textContainer);
+        }
+
+        public void StartRename(Action onRenameFinished)
+        {
+            var textField = new TextField
+            {
+                value = _label?.text
+            };
+            textField.AddToClassList(TextInputUssClassName);
+            
+            _textContainer.Add(textField);
+            
+            // すぐにFocusするとWarningが出るので少し遅らせる @Unity6000.0.2f1
+            schedule.Execute(() => textField.Focus()).ExecuteLater(32);
+
+            DisplayLabel(false);
+            
+            textField.RegisterCallback<BlurEvent>(OnBlur);
+            RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
+            
+            return;
+            
+
+            void OnBlur(BlurEvent evt)
+            {
+                StopRename();
+                
+                Label = textField.value;
+                onRenameFinished?.Invoke();
+            }
+
+            void OnDetachFromPanel(DetachFromPanelEvent evt)
+            {
+                StopRename();
+            }
+            
+            void StopRename()
+            {
+                textField.UnregisterCallback<BlurEvent>(OnBlur);
+                UnregisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
+
+                _textContainer.Remove(textField);
+                DisplayLabel(true);
+            }
+            
+            void DisplayLabel(bool display)
+            {
+                if (_label == null) return;
+                _label.style.display = display ? DisplayStyle.Flex : DisplayStyle.None;
+            }
         }
     }
 }
--- Packages/RosettaUI.UIToolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatchSet.cs
+++ Packages/RosettaUI.UIToolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatchSet.cs
@@ -32,7 +32,14 @@ namespace RosettaUI.UIToolkit
 
         private readonly Action<Color> _setColorPickerColor; 
         private readonly ColorPickerSwatch _currentSwatch;
-  
+
+        
+        private bool IsSwatchDisplayLabel => Layout == TileLayout.List;
+        
+        private IEnumerable<ColorPickerSwatch> SwatchesWithCurrent => Children().Select(v => v as ColorPickerSwatch);
+        private IEnumerable<ColorPickerSwatch> Swatches => SwatchesWithCurrent.Where(s => s != _currentSwatch);
+        
+
         public TileLayout Layout
         {
             get => ClassListContains(GridUssClassName) ? TileLayout.Grid : TileLayout.List;
@@ -42,15 +49,18 @@ namespace RosettaUI.UIToolkit
                 var isGrid = value == TileLayout.Grid;
                 EnableInClassList(GridUssClassName, isGrid);
                 EnableInClassList(ListUssClassName, !isGrid);
+                
+                UpdateSwatchesEnableText();
             }
         }
-  
+
         public ColorPickerSwatchSet(Action<Color> setColorPickerColor)
         {
             _setColorPickerColor = setColorPickerColor;
             text = "Swatches";
             
             AddToClassList(UssClassName);
+            AddToClassList(ListUssClassName);
 
             _swatchSetMenu = CreateSwatchSetMenu();
             var toggle = this.Q<Toggle>();
@@ -67,6 +77,8 @@ namespace RosettaUI.UIToolkit
             Add(_currentSwatch);
             
             LoadSwatches();
+            
+            UpdateSwatchesEnableText();
         }
 
         private VisualElement CreateSwatchSetMenu()
@@ -92,13 +104,18 @@ namespace RosettaUI.UIToolkit
    
         public void SetColor(Color color) => _currentSwatch.Color = color;
 
-        private void SetMenuVisible(bool visible) => _swatchSetMenu.style.display = visible ? DisplayStyle.Flex : DisplayStyle.None;
+        private void SetMenuVisible(bool v) => _swatchSetMenu.style.display = v ? DisplayStyle.Flex : DisplayStyle.None;
         
     
         private void OnCurrentSwatchPointerDown(PointerDownEvent evt)
         {
-            AddSwatch(_currentSwatch.Color);
+            var newSwatch = AddSwatch(_currentSwatch.Color);
             SaveSwatches();
+            
+            if ( IsSwatchDisplayLabel )
+            {
+                newSwatch.StartRename(SaveSwatches);
+            }
             
             evt.StopPropagation();
         }
@@ -114,29 +131,53 @@ namespace RosettaUI.UIToolkit
                     evt.StopPropagation();
                     break;
                 case 1:
-                    PopupMenu.Show(new[]
-                        {
-                            new MenuItem("Replace", () => ReplaceSwatchColorToCurrent(swatch)),
-                            new MenuItem("Delete", () => DeleteSwatch(swatch)),
-                            new MenuItem("Move To First", () => MoveToFirstSwatch(swatch)),
-                        }, 
+                    PopupMenu.Show(
+                        CreateMenuItems(),
                         evt.position, 
                         swatch);
                 
                     evt.StopPropagationAndFocusControllerIgnoreEvent();
                     break;
             }
-        }
 
-        private void AddSwatch(Color color)
+            return;
+
+            IEnumerable<MenuItem> CreateMenuItems()
+            {
+                yield return new MenuItem("Replace", () => ReplaceSwatchColorToCurrent(swatch));
+                yield return new MenuItem("Delete", () => DeleteSwatch(swatch));
+                if (Layout == TileLayout.List)
+                {
+                    yield return new MenuItem("Rename", () => swatch.StartRename(SaveSwatches));
+                }
+                yield return new MenuItem("Move To First", () => MoveToFirstSwatch(swatch));
+            }
+        }
+        
+        
+        private void UpdateSwatchesEnableText()
+        {
+            var isSwatchLabelVisible = IsSwatchDisplayLabel;
+            
+            foreach (var swatch in SwatchesWithCurrent)
+            {
+                swatch.EnableText = isSwatchLabelVisible;
+            }
+        }
+        
+
+        private ColorPickerSwatch AddSwatch(Color color, string swatchName = null)
         {
             var swatch = new ColorPickerSwatch
             {
+                Label = swatchName,
                 Color = color,
-                Label = color.ToString()
             };
             swatch.RegisterCallback<PointerDownEvent>(OnSwatchPointerDown);
+            swatch.EnableText = IsSwatchDisplayLabel;
             Insert(childCount - 1, swatch);
+
+            return swatch;
         }
 
         private void DeleteSwatch(ColorPickerSwatch swatch)
@@ -158,13 +199,10 @@ namespace RosettaUI.UIToolkit
             SaveSwatches();
         }
         
-
         private void SaveSwatches()
         {
-            var swatches = Children().Select(v => v as ColorPickerSwatch).Where(s => s != _currentSwatch);
-
             using var _ = ListPool<NameAndColor>.Get(out var nameAndColors);
-            nameAndColors.AddRange(swatches.Select(s => new NameAndColor { name = null, color = s.Color }));
+            nameAndColors.AddRange(Swatches.Select(s => new NameAndColor { name = s.Label, color = s.Color }));
             
             PersistantData.Set(ColorPickerSwatchesKey, nameAndColors);
         }
@@ -176,7 +214,7 @@ namespace RosettaUI.UIToolkit
             {
                 foreach (var nameAndColor in nameAndColors)
                 {
-                    AddSwatch(nameAndColor.color);
+                    AddSwatch(nameAndColor.color, nameAndColor.name);
                 }
             }
         }

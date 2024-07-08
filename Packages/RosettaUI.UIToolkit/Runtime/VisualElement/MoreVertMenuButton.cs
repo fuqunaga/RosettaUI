ceb76766c0e031ca16f2b9dd9a8456a743a4760c
fuqunaga <fukhid@gmail.com>
2024-07-08 17:11:47 +0900

    add MoveVertButton

---
+++ Packages/RosettaUI.UIToolkit/Runtime/VisualElement/MoreVertMenuButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace RosettaUI.UIToolkit
+{
+    public class MoreVertMenuButton : Button
+    {
+        public const string UssClassName = "rosettaui-morevert-menu-button";
+
+        public int ButtonIndex { get; set; } = 1;
+        public Func<IEnumerable<MenuItem>> CreateMenuItems { get; set; }
+
+        public MoreVertMenuButton()
+        {
+            AddToClassList(UssClassName);
+
+            clickable = new Clickable(OnClick);
+        }
+
+        private void OnClick(EventBase evtBase)
+        {
+            if (evtBase is not PointerUpEvent evt) return;
+            
+            var menuItems = CreateMenuItems?.Invoke();
+            if (menuItems == null) return;
+                
+            PopupMenu.Show(menuItems, evt.position, this);
+
+            evt.StopPropagationAndFocusControllerIgnoreEvent();
+        }
+    }
+}

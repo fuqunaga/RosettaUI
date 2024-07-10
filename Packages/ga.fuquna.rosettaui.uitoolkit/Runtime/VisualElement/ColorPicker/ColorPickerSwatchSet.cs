ed17b947e06bf4123898cfd81d84186180e309d7
fuqunaga <fukhid@gmail.com>
2024-07-10 16:42:47 +0900

    Swatch scrollview size

--- Packages/ga.fuquna.rosettaui.uitoolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatchSet.cs
+++ Packages/ga.fuquna.rosettaui.uitoolkit/Runtime/VisualElement/ColorPicker/ColorPickerSwatchSet.cs
@@ -5,10 +5,12 @@ namespace RosettaUI.UIToolkit
 {
     public class ColorPickerSwatchSet : SwatchSetBase<Color, ColorPickerSwatch>
     {
+        public new const string UssClassName = "rosettaui-colorpicker-swatchset";
         public const string KeyPrefix = "RosettaUI-ColorPickerSwatchSet";
         
         public ColorPickerSwatchSet(Action<Color> applyValueFunc) : base("Swatches", applyValueFunc)
         {
+            AddToClassList(UssClassName);
         }
 
         protected override string DataKeyPrefix => KeyPrefix;

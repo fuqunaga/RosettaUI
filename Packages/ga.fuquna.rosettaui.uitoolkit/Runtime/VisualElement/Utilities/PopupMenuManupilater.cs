e7a925485cdf4fe5666117846862422b84cae41c
fuqunaga <fukhid@gmail.com>
2024-07-11 18:11:47 +0900

    Merge PopupMenu with overlapping PopupMenu to display the menu.

---
+++ Packages/ga.fuquna.rosettaui.uitoolkit/Runtime/VisualElement/Utilities/PopupMenuManupilater.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
+
+namespace RosettaUI.UIToolkit
+{
+    public class PopupMenuManipulator : Clickable
+    {
+        private readonly Func<IEnumerable<MenuItem>> _createMenuItemsFunc;
+
+        public MouseButton Button
+        {
+            set
+            {
+                activators.Clear();
+                activators.Add(new ManipulatorActivationFilter {button = value});
+            }
+        }
+
+        public PopupMenuManipulator(Func<IEnumerable<MenuItem>> createMenuItemsFunc) : base((Action)null)
+        {
+            clickedWithEventInfo += OnClick;
+            _createMenuItemsFunc = createMenuItemsFunc;
+            Button = MouseButton.RightMouse;
+        }
+
+        private void OnClick(EventBase evt)
+        {
+            var success = evt.TryGetPosition(out var position);
+            if (!success) return;
+
+            if (evt.target is VisualElement visualElement)
+            {
+                PopupMenuEvent.Send(visualElement, position);
+            }
+        }
+
+        protected override void RegisterCallbacksOnTarget()
+        {
+            base.RegisterCallbacksOnTarget();
+
+            target.RegisterCallback<PopupMenuEvent>(OnPopupMenuTrickleDown, TrickleDown.TrickleDown);
+            target.RegisterCallback<PopupMenuEvent>(OnPopupMenuBubbleUp);
+        }
+
+
+        protected override void UnregisterCallbacksFromTarget()
+        {
+            base.UnregisterCallbacksFromTarget();
+
+            target.UnregisterCallback<PopupMenuEvent>(OnPopupMenuTrickleDown);
+            target.UnregisterCallback<PopupMenuEvent>(OnPopupMenuBubbleUp);
+        }
+
+
+        private void OnPopupMenuTrickleDown(PopupMenuEvent evt)
+        {
+            var menuItems = evt.MenuItems;
+            
+            if (menuItems.Any())
+            {
+                menuItems.Add(MenuItem.Separator);
+            }
+            
+            evt.MenuItems.AddRange(_createMenuItemsFunc());
+        }
+
+
+        private void OnPopupMenuBubbleUp(PopupMenuEvent evt)
+        {
+            if (evt.target != target) return;
+            
+            PopupMenuUtility.Show(
+                evt.MenuItems,
+                evt.Position,
+                target
+            );
+            
+            evt.StopImmediatePropagation();
+        }
+    }
+}

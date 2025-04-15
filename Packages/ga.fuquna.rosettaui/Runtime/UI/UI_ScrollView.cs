b392fc79eff694787e998ffb990f3c1e7acfd57d
fuqunaga <fukhid@gmail.com>
2025-04-15 19:16:34 +0900

    feat: Update UI.ScrollView to treat received size as MaxWidth/MaxHeight

--- Packages/ga.fuquna.rosettaui/Runtime/UI/UI_ScrollView.cs
+++ Packages/ga.fuquna.rosettaui/Runtime/UI/UI_ScrollView.cs
@@ -5,23 +5,23 @@ namespace RosettaUI
 {
     public static partial class UI
     {
-        public static ScrollViewElement ScrollViewVertical(float? height, params Element[] elements) 
-            => ScrollViewVertical(height, elements.AsEnumerable());
+        public static ScrollViewElement ScrollViewVertical(float? maxHeight, params Element[] elements) 
+            => ScrollViewVertical(maxHeight, elements.AsEnumerable());
 
-        public static ScrollViewElement ScrollViewVertical(float? height, IEnumerable<Element> elements)
-            => ScrollView(ScrollViewType.Vertical, elements).SetHeight(height);
+        public static ScrollViewElement ScrollViewVertical(float? maxHeight, IEnumerable<Element> elements)
+            => ScrollView(ScrollViewType.Vertical, elements).SetMaxHeight(maxHeight);
 
-        public static ScrollViewElement ScrollViewHorizontal(float? width, params Element[] elements) 
-            => ScrollViewHorizontal(width, elements.AsEnumerable());
+        public static ScrollViewElement ScrollViewHorizontal(float? maxWidth, params Element[] elements) 
+            => ScrollViewHorizontal(maxWidth, elements.AsEnumerable());
         
-        public static ScrollViewElement ScrollViewHorizontal(float? width, IEnumerable<Element> elements)
-            => ScrollView(ScrollViewType.Horizontal, elements).SetWidth(width);
+        public static ScrollViewElement ScrollViewHorizontal(float? maxWidth, IEnumerable<Element> elements)
+            => ScrollView(ScrollViewType.Horizontal, elements).SetMaxWidth(maxWidth);
         
-        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float? width, float? height, params Element[] elements) 
-            => ScrollViewVerticalAndHorizontal(width, height, elements.AsEnumerable());
+        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float? maxWidth, float? maxHeight, params Element[] elements) 
+            => ScrollViewVerticalAndHorizontal(maxWidth, maxHeight, elements.AsEnumerable());
         
-        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float? width, float? height, IEnumerable<Element> elements)
-            => ScrollView(ScrollViewType.VerticalAndHorizontal, elements).SetWidth(width).SetHeight(height);
+        public static ScrollViewElement ScrollViewVerticalAndHorizontal(float? maxWidth, float? maxHeight, IEnumerable<Element> elements)
+            => ScrollView(ScrollViewType.VerticalAndHorizontal, elements).SetMaxWidth(maxWidth).SetMaxHeight(maxHeight);
         
         
         public static ScrollViewElement ScrollView(ScrollViewType scrollViewType, IEnumerable<Element> elements) 

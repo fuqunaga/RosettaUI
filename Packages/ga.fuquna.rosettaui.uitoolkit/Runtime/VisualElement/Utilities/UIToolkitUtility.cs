c3772df08d57e9e5d85ce008aa3d1092ff71aaf9
fuqunaga <fukhid@gmail.com>
2025-07-02 19:04:52 +0900

    perf: remove UIToolkitUtility.WillUseKeyInput allocation

--- Packages/ga.fuquna.rosettaui.uitoolkit/Runtime/VisualElement/Utilities/UIToolkitUtility.cs
+++ Packages/ga.fuquna.rosettaui.uitoolkit/Runtime/VisualElement/Utilities/UIToolkitUtility.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using UnityEngine.Assertions;
 using UnityEngine.UIElements;
@@ -6,7 +7,9 @@ namespace RosettaUI.UIToolkit
 {
     public  static class UIToolkitUtility
     {
-        private static MethodInfo _focusControllerGetLeafFocusedElementMethodInfo;
+        private delegate Focusable GetLeafFocusedElementDelegate(FocusController focusController);
+        
+        private static GetLeafFocusedElementDelegate _focusControllerGetLeafFocusedElementDelegate;
 #if UNITY_2023_1_OR_NEWER
         private static PropertyInfo _baseBoolFieldAcceptClicksIfDisabledPropertyInfo;
 #else
@@ -16,14 +19,19 @@ namespace RosettaUI.UIToolkit
         
         public static bool WillUseKeyInput(IPanel panel)
         {
-            _focusControllerGetLeafFocusedElementMethodInfo ??= typeof(FocusController).GetMethod("GetLeafFocusedElement", BindingFlags.NonPublic | BindingFlags.Instance);
-            Assert.IsNotNull(_focusControllerGetLeafFocusedElementMethodInfo);
+            if (_focusControllerGetLeafFocusedElementDelegate == null)
+            {
+                var methodInfo = typeof(FocusController).GetMethod("GetLeafFocusedElement", BindingFlags.NonPublic | BindingFlags.Instance);
+                Assert.IsNotNull(methodInfo);
+
+                _focusControllerGetLeafFocusedElementDelegate ??= (GetLeafFocusedElementDelegate)Delegate.CreateDelegate(typeof(GetLeafFocusedElementDelegate), methodInfo);
+                Assert.IsNotNull(_focusControllerGetLeafFocusedElementDelegate);    
+            }
             
             var focusController = panel?.focusController;
             if (focusController == null) return false;
             
-            var focusable = _focusControllerGetLeafFocusedElementMethodInfo.Invoke(focusController, null) as Focusable;
-
+            var focusable = _focusControllerGetLeafFocusedElementDelegate(focusController);
             return focusable is ITextEdition { isReadOnly: false};
         }
 

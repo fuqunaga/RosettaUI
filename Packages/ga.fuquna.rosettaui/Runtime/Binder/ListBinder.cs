1d16c752154ddd131514830c1edcbc45c2dc2c13
fuqunaga <fukhid@gmail.com>
2025-10-28 19:31:59 +0900

    ListBinder made struct to have members.

--- Packages/ga.fuquna.rosettaui/Runtime/Binder/ListBinder.cs
+++ Packages/ga.fuquna.rosettaui/Runtime/Binder/ListBinder.cs
@@ -1,138 +1,81 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine.Assertions;
 
 namespace RosettaUI
 {
-    public static class ListBinder
+    using CreateItemInstanceFunc = Func<IList, Type, int, object>;
+
+    public readonly partial struct ListBinder
     {
-        public static Type GetItemBinderType(Type type)
-        {
-            var listType = IsIList(type)
-                ? type
-                : type.GetInterfaces().FirstOrDefault(IsIList);
-            Assert.IsNotNull(listType, $"{type} does not Inherit from IList<>.");
-            
-            var itemType =  listType.GetGenericArguments()[0];
-            return typeof(ListItemBinder<>).MakeGenericType(itemType);
+        private readonly IBinder _binder;
+        private readonly CreateItemInstanceFunc _createItemInstanceFunc;
 
-            static bool IsIList(Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IList<>);
-        }
-
-        public static IListItemBinder CreateItemBinderAt(IBinder listBinder, int index)
-        {
-            var itemBinderType = GetItemBinderType(listBinder.ValueType);
-            return CreateItemBinderAt(listBinder, index, itemBinderType);
-        }
-
-        private static IListItemBinder CreateItemBinderAt(IBinder listBinder, int index, Type itemBinderType)
-            => Activator.CreateInstance(itemBinderType, listBinder, index) as IListItemBinder;
-
-        public static IList GetIList(IBinder binder) => binder.GetObject() as IList;
+        public Type ListType => _binder.ValueType;
+        public Type ItemType => ListUtility.GetItemType(ListType);
         
-        public static int GetCount(IBinder binder)
+        public ListBinder(IBinder binder, CreateItemInstanceFunc createItemInstanceFunc)
         {
-            return GetIList(binder)?.Count ?? 0;
+            Assert.IsTrue(IsListBinder(binder));
+
+            _binder = binder;
+            _createItemInstanceFunc = createItemInstanceFunc;
         }
 
-        public static void SetCount(IBinder binder, int count)
+        public IListItemBinder CreateItemBinderAt(int index) => CreateItemBinderAt(_binder, index);
+        public IList GetIList() => GetIList(_binder);
+
+        public int GetCount() => GetCount(_binder);
+
+        public void SetCount(int count)
         {
-            var current = GetCount(binder);
+            var current = GetCount();
             var diff = count - current;
             for (var i = 0; i < diff; ++i)
             {
-                AddItemAtLast(binder);
+                AddItemAtLast();
             }
 
             for (var i = 0; i < -diff; ++i)
             {
-                RemoveItemAtLast(binder);
+                RemoveItemAtLast();
             }
         }
 
-        public static bool IsListBinder(IBinder binder)
+        public bool IsReadOnly() => IsReadOnly(_binder);
+
+        public void DuplicateItem(int index) => DuplicateItem(_binder, index);
+        public void AddItem(int index) => AddItem(_binder, index);
+        public void AddNullItem(int index) => AddNullItem(_binder, index);
+        public void RemoveItem(int index) => RemoveItem(_binder, index);
+        public void RemoveItems(Range range) => RemoveItems(_binder, range);
+        public void MoveItem(int fromIndex, int toIndex) => MoveItem(_binder, fromIndex, toIndex);
+
+        public void AddItemAtLast()
         {
-            var type = binder.ValueType;
-            return typeof(IList).IsAssignableFrom(type)
-                && type.GetInterfaces()
-                    .Where(t => t.IsGenericType)
-                    .Select(t => t.GetGenericTypeDefinition())
-                    .Contains(typeof(IList<>));
-        }
-
-        public static bool IsReadOnly(IBinder binder) => binder.IsReadOnly || (GetIList(binder)?.IsReadOnly ?? false);
-
-
-        public static void DuplicateItem(IBinder binder, int index)
-        {
-            var list = GetIList(binder);
+            var list = GetIList() ?? (IList) Activator.CreateInstance(ListType);
             
-            var itemType = ListUtility.GetItemType(binder.ValueType);
-            
-            list = ListUtility.DuplicateItem(list, itemType, list[index], index + 1);
-            binder.SetObject(list);
-        }
+            var index = list?.Count ?? 0;
+            var newItem = CreateNewItem(list, index);
 
-        public static void AddItem(IBinder binder, int index)
-        {
-            var list = GetIList(binder);
+            list = ListUtility.AddItem(list, ItemType, newItem, index);
             
-            var itemType = ListUtility.GetItemType(binder.ValueType);
-            
-            list = ListUtility.DuplicateItem(list, itemType, null, index);
-            binder.SetObject(list);
+            _binder.SetObject(list);
         }
         
-        public static void AddNullItem(IBinder binder, int index)
-        {
-            var list = GetIList(binder);
-            
-            var itemType = ListUtility.GetItemType(binder.ValueType);
-            
-            list = ListUtility.AddNullItem(list, itemType, index);
-            binder.SetObject(list);
-        }
-
-        public static void RemoveItem(IBinder binder, int index)
-            => RemoveItems(binder, index..(index + 1));
+        public void RemoveItemAtLast() => RemoveItemAtLast(_binder);
         
-        public static void RemoveItems(IBinder binder, Range range)
-        {
-            var list = GetIList(binder);
-            
-            var itemType = ListUtility.GetItemType(binder.ValueType);
-            
-            list = ListUtility.RemoveItems(list, itemType, range);
-            binder.SetObject(list);
-        }
         
-        public static void MoveItem(IBinder binder, int fromIndex, int toIndex)
+        private object CreateNewItem(IList list, int index)
         {
-            var list = GetIList(binder);
-            ListUtility.MoveItem(list, fromIndex, toIndex);
-            binder.SetObject(list);
-        }
-
-        public static void AddItemAtLast(IBinder binder)
-        {
-            var list = GetIList(binder);
+            if (_createItemInstanceFunc != null)
+            {
+                return _createItemInstanceFunc(list, ItemType, index);
+            }
             
-            var listType = binder.ValueType;
-            var itemType = ListUtility.GetItemType(binder.ValueType);
-
-            list = ListUtility.AddItemAtLast(list, listType, itemType);
-            binder.SetObject(list);
-        }
-
-        public static void RemoveItemAtLast(IBinder binder)
-        {
-            var list = GetIList(binder);
-            
-            var itemType = ListUtility.GetItemType(binder.ValueType);
-            list = ListUtility.RemoveItemAtLast(list, itemType);
-            binder.SetObject(list);
+            var baseIndex = Math.Max(0, list.Count - 1);
+            var baseItem = list.Count > 0 ? list[baseIndex] : null;
+            return ListUtility.CreateNewItem(baseItem, ItemType);
         }
     }
 }
---
+++ Packages/ga.fuquna.rosettaui/Runtime/Binder/ListBinder_Static.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace RosettaUI
+{
+    public readonly partial struct ListBinder
+    {
+        public static Type GetItemBinderType(Type type)
+        {
+            var listType = IsIList(type)
+                ? type
+                : type.GetInterfaces().FirstOrDefault(IsIList);
+            Assert.IsNotNull(listType, $"{type} does not Inherit from IList<>.");
+            
+            var itemType =  listType.GetGenericArguments()[0];
+            return typeof(ListItemBinder<>).MakeGenericType(itemType);
+
+            static bool IsIList(Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IList<>);
+        }
+
+        public static IListItemBinder CreateItemBinderAt(IBinder listBinder, int index)
+        {
+            var itemBinderType = GetItemBinderType(listBinder.ValueType);
+            return CreateItemBinderAt(listBinder, index, itemBinderType);
+        }
+
+        private static IListItemBinder CreateItemBinderAt(IBinder listBinder, int index, Type itemBinderType)
+            => Activator.CreateInstance(itemBinderType, listBinder, index) as IListItemBinder;
+
+        public static IList GetIList(IBinder binder) => binder.GetObject() as IList;
+        
+        public static int GetCount(IBinder binder)
+        {
+            return GetIList(binder)?.Count ?? 0;
+        }
+
+        public static void SetCount(IBinder binder, int count)
+        {
+            var current = GetCount(binder);
+            var diff = count - current;
+            for (var i = 0; i < diff; ++i)
+            {
+                AddItemAtLast(binder);
+            }
+
+            for (var i = 0; i < -diff; ++i)
+            {
+                RemoveItemAtLast(binder);
+            }
+        }
+
+        public static bool IsListBinder(IBinder binder)
+        {
+            var type = binder.ValueType;
+            return typeof(IList).IsAssignableFrom(type)
+                && type.GetInterfaces()
+                    .Where(t => t.IsGenericType)
+                    .Select(t => t.GetGenericTypeDefinition())
+                    .Contains(typeof(IList<>));
+        }
+
+        public static bool IsReadOnly(IBinder binder) => binder.IsReadOnly || (GetIList(binder)?.IsReadOnly ?? false);
+
+
+        public static void DuplicateItem(IBinder binder, int index)
+        {
+            var list = GetIList(binder);
+            
+            var itemType = ListUtility.GetItemType(binder.ValueType);
+            var newItem = ListUtility.CreateNewItem(list[index], itemType);
+            list = ListUtility.AddItem(list, itemType, newItem, index + 1);
+            binder.SetObject(list);
+        }
+
+        public static void AddItem(IBinder binder, int index)
+        {
+            var list = GetIList(binder);
+            
+            var itemType = ListUtility.GetItemType(binder.ValueType);
+            var newItem = ListUtility.CreateNewItem(null, itemType);
+            
+            list = ListUtility.AddItem(list, itemType, newItem, index);
+            binder.SetObject(list);
+        }
+        
+        public static void AddNullItem(IBinder binder, int index)
+        {
+            var list = GetIList(binder);
+            
+            var itemType = ListUtility.GetItemType(binder.ValueType);
+            
+            list = ListUtility.AddNullItem(list, itemType, index);
+            binder.SetObject(list);
+        }
+
+        public static void RemoveItem(IBinder binder, int index)
+            => RemoveItems(binder, index..(index + 1));
+        
+        public static void RemoveItems(IBinder binder, Range range)
+        {
+            var list = GetIList(binder);
+            
+            var itemType = ListUtility.GetItemType(binder.ValueType);
+            
+            list = ListUtility.RemoveItems(list, itemType, range);
+            binder.SetObject(list);
+        }
+        
+        public static void MoveItem(IBinder binder, int fromIndex, int toIndex)
+        {
+            var list = GetIList(binder);
+            ListUtility.MoveItem(list, fromIndex, toIndex);
+            binder.SetObject(list);
+        }
+
+        public static void AddItemAtLast(IBinder binder)
+        {
+            var list = GetIList(binder);
+            
+            var listType = binder.ValueType;
+            var itemType = ListUtility.GetItemType(binder.ValueType);
+
+            list = ListUtility.AddItemAtLast(list, listType, itemType);
+            binder.SetObject(list);
+        }
+
+        public static void RemoveItemAtLast(IBinder binder)
+        {
+            var list = GetIList(binder);
+            
+            var itemType = ListUtility.GetItemType(binder.ValueType);
+            list = ListUtility.RemoveItemAtLast(list, itemType);
+            binder.SetObject(list);
+        }
+    }
+}
